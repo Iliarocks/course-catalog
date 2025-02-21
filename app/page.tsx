199c2bb2ac751e027a5b7d3499ef005293d73b89
Iliarocks <parun.ilia@gmail.com>
2025-02-21 14:32:31 -0800

    Fixed build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import React from "react";
-import { init } from "@instantdb/react";
-import schema from "../instant.schema";
+// import { init } from "@instantdb/react";
+// import schema from "../instant.schema";
 
-const db = init({ appId: "d61474bf-3716-48ff-a937-160d78848b7f", schema });
+// const db = init({ appId: "d61474bf-3716-48ff-a937-160d78848b7f", schema });
 
 export default function Home() {
   return <main className="max-w-screen-sm m-auto">Home</main>;
