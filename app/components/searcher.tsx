0859f4e5e43b72677f85ac440ce2f4d4db40c814
Iliarocks <parun.ilia@gmail.com>
2025-03-02 18:34:47 -0800

    Added course page and desktop css

--- app/components/searcher.tsx
+++ app/components/searcher.tsx
@@ -27,7 +27,7 @@ export default function Searcher({
           ],
           // ...(level ? { code: { $ilike: `${level}%` } } : {}),
         },
-        limit: 300,
+        limit: 400,
       },
     },
   });
@@ -40,47 +40,49 @@ export default function Searcher({
   if (isLoading || !data || !data.courses) return;
 
   return (
-    <ul className="flex h-full w-full flex-col gap-sm overflow-y-scroll p-md">
+    <ul className="py-auto flex h-full w-full flex-col gap-sm overflow-y-scroll p-md md:gap-md md:p-lg lg:grid lg:grid-cols-2 lg:px-[70px]">
       {(level || credits
         ? data.courses.filter(
             (c) => c.code.startsWith(level) && c.credits.startsWith(credits),
           )
         : data.courses
-      ).map((course, index) => (
-        <article
-          key={index}
-          className="flex flex-col gap-xs rounded border-2 border-fifth p-sm shadow-custom"
-        >
-          <div className="flex w-full justify-between">
-            <div className="max-w-[80%]">
-              <p className="text-xs text-first">{`${course.subject} ${course.code}`}</p>
-              <p className="truncate text-2xs text-second">{`${course.name}`}</p>
-              <p className="text-2xs text-fourth">{`${course.credits} Credits`}</p>
-            </div>
-            <div className="grid h-fit w-fit place-items-center rounded bg-fifth p-xs">
-              <Link
-                href={`/course/${(course.subject + course.code).toLowerCase()}`}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-4 text-fourth"
+      )
+        .slice(0, limit)
+        .map((course, index) => (
+          <article
+            key={index}
+            className="flex flex-col gap-xs rounded border-2 border-fifth p-sm shadow-custom md:p-[15px]"
+          >
+            <div className="flex w-full justify-between">
+              <div className="max-w-[80%]">
+                <p className="text-xs font-medium text-first md:text-sm">{`${course.subject} ${course.code}`}</p>
+                <p className="truncate text-2xs font-medium text-second md:text-xs">{`${course.name}`}</p>
+                <p className="text-2xs font-medium text-fourth md:text-xs">{`${course.credits} Credits`}</p>
+              </div>
+              <div className="grid h-fit w-fit place-items-center rounded bg-fifth p-xs duration-100 hover:shadow-custom">
+                <Link
+                  href={`/course/${(course.subject + course.code).toLowerCase()}`}
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
-                  />
-                </svg>
-              </Link>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-4 text-fourth lg:size-5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
+                    />
+                  </svg>
+                </Link>
+              </div>
             </div>
-          </div>
-          <p className="line-clamp-2 text-2xs text-fourth">{`${course.description}`}</p>
-        </article>
-      ))}
+            <p className="line-clamp-2 text-2xs text-fourth md:text-sm">{`${course.description}`}</p>
+          </article>
+        ))}
     </ul>
   );
 }
---
+++ app/course/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import React from "react";
+import { init } from "@instantdb/react";
+import schema from "../../instant.schema";
+import { usePathname } from "next/navigation";
+
+const db = init({ appId: "d61474bf-3716-48ff-a937-160d78848b7f", schema });
+
+export default function Auth() {
+  const pathname = usePathname();
+  const coursePath = pathname.split("/").pop();
+  const courseSubject = coursePath.substring(0, 4);
+  const courseCode = coursePath.substring(4, 8);
+
+  const { isLoading, error, data } = db.useQuery({
+    courses: {
+      $: {
+        where: {
+          subject: courseSubject.toUpperCase(),
+          code: courseCode,
+        },
+      },
+    },
+  });
+
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  if (isLoading || !data || !data.courses || !data.courses[0]) return;
+
+  const course = data.courses[0];
+
+  return (
+    <div className="flex h-full w-full flex-col">
+      <section className="h-fit min-h-[150px] w-full bg-gradient-to-tr from-fifth from-40% via-third via-70% to-fifth to-100% shadow-custom">
+        <header className="flex h-full w-full flex-col justify-center gap-xs p-md backdrop-blur-lg backdrop-brightness-105 backdrop-saturate-[0.75] md:gap-xs md:p-lg lg:px-[70px] lg:py-[50px]">
+          <h1 className="text-xl font-semibold text-first md:text-3xl">
+            {course.name}
+          </h1>
+          <h3 className="text-sm font-medium text-second md:text-base lg:text-lg">{`${course.subject} ${course.code} · ${course.credits} Credits`}</h3>
+          <p className="text-xs text-fourth md:text-sm lg:text-base">
+            {course.description}
+          </p>
+        </header>
+      </section>
+    </div>
+  );
+}
